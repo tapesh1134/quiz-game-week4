d7eb6e68a458a7a7e9edfb87f815c2c3c03acb51
tapesh1134 <tapeshs1134@gmail.com>
2025-02-03 20:04:29 +0530

    wrap up code

--- server.js
+++ server.js
@@ -19,3 +19,28 @@ const server = http.createServer((req, res) => {
         serverfile(res, public${req.url}, contenttype(req.url));
     }
 });
+
+function serverfile(res, filePath, contentType) {
+    fs.readFile(path.join(__dirname, filePath), (err, content) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Not Found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content);
+        }
+    });
+}
+
+function contenttype(url) {
+    const ext = path.extname(url);
+    switch (ext) {
+        case '.css': return 'text/css';
+        case '.js': return 'application/javascript';
+        case '.json': return 'application/json';
+        default: return 'text/html';
+    }
+}
+
+const PORT = 8000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
